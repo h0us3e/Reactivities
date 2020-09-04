eb1091e2de09f2960b87753511662d37f2cf2f9d
Atanas Ivanov <a.ivanov9393@gmail.com>
2020-09-04 11:35:39 +0200

    End of section 10

--- Application/Activities/Delete.cs
+++ Application/Activities/Delete.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Errors;
 using MediatR;
 using Persistence;
 
@@ -28,7 +30,7 @@ namespace Application.Activities
         var activity = await _context.Activities.FindAsync(request.Id);
 
         if (activity == null)
-          throw new Exception("Could not find activity");
+          throw new RestException(HttpStatusCode.NotFound, new { activity = "Not Found" });
 
         _context.Remove(activity);
         //handler logic
@@ -36,7 +38,7 @@ namespace Application.Activities
 
         if (success) return Unit.Value;
 
-        throw new Exception("There was a problem");
+        throw new Exception("There was a problem saving changes");
       }
     }
   }
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "react-toastify/dist/ReactToastify.min.css";
 import "./app/layout/style.css";
 import App from "./app/layout/App";
 import * as serviceWorker from "./serviceWorker";
 import "semantic-ui-css/semantic.min.css";
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import { Router } from "react-router-dom";
 import ScrollToTop from "../src/app/layout/ScrollToTop";
 
+export const history = createBrowserHistory();
+
 ReactDOM.render(
-  <BrowserRouter>
+  <Router history={history}>
     <ScrollToTop>
       <App />
     </ScrollToTop>
-  </BrowserRouter>,
+  </Router>,
   document.getElementById("root")
 );
 
